4d6d36277a85e2fbc8e6deb27f506456447ce4aa
DanielWisn <u145_danwis_waw@technischools.com>
2025-05-24 18:38:44 +0200

    a*

--- main.py
+++ main.py
@@ -106,13 +106,13 @@ class Maze:
 
 
 class Solver:
-    def __init__(self, maze:Maze):
+    def __init__(self, maze:Maze) -> None:
         self.maze = maze
         self.playerX = self.maze.startX
         self.playerY = self.maze.startY
         self.current_direction: Literal["up", "down", "left", "right"] = "left"
 
-    def right_hand_solver(self):
+    def right_hand_solver(self) -> None:
         while self.maze.check_win(self.playerX,self.playerY) != True:
             current_rigth = ""
             current_forward = ""
@@ -160,7 +160,7 @@ class Solver:
 
             self.maze.draw_maze(self.playerX,self.playerY)
 
-    def dfs_solver(self):
+    def dfs_solver(self) -> None:
         stack = []
         neighbors = [(self.playerX,self.playerY-1)]
         while self.maze.check_win(self.playerX,self.playerY) != True:
@@ -182,7 +182,7 @@ class Solver:
             neighbors = [(self.playerX,self.playerY-1),(self.playerX-1,self.playerY),(self.playerX+1,self.playerY),(self.playerX,self.playerY+1)]
             self.maze.draw_maze(self.playerX,self.playerY)
     
-    def bfs_solver(self):
+    def bfs_solver(self) -> None:
         queue = []
         parents = {}
         neighbors = [(self.playerX,self.playerY-1,(self.playerX,self.playerY))]
@@ -213,7 +213,7 @@ class Solver:
             except:
                 break
         
-    def dead_end_solver(self):
+    def dead_end_solver(self) -> None:
         free_paths = []
         cells_to_check = {}
         path_created = False
@@ -252,7 +252,7 @@ class Solver:
                 free_paths.remove(i)
                 cells_to_check.pop(i)
         
-    def tremaux_solver(self):
+    def tremaux_solver(self) -> None:
         self.dont = True
         self.playerX,self.playerY = self.playerX,self.playerY-1
         neighbors = [(self.playerX,self.playerY-1),(self.playerX-1,self.playerY),(self.playerX+1,self.playerY),(self.playerX,self.playerY+1)]
@@ -298,6 +298,107 @@ class Solver:
             self.maze.draw_maze(self.playerX,self.playerY,True)
             neighbors = [(self.playerX,self.playerY-1),(self.playerX-1,self.playerY),(self.playerX+1,self.playerY),(self.playerX,self.playerY+1)]
 
+    def a_star(self) -> None:
+        class PriorityQueue:
+            def __init__(self, startX, startY, exitX, exitY) -> None:
+                self.goalX = exitX
+                self.goalY = exitY
+                self.path = {} 
+                self.path_cost = {(startX, startY): 0}
+                self.to_explore = [((startX, startY), self.heuristic((startX, startY)))]
+
+            def heuristic(self, node: tuple) -> int:
+                return abs(node[0] - self.goalX) + abs(node[1] - self.goalY)
+
+            def add_element(self, node: tuple):
+                x, y, parent = node[0], node[1], node[2]
+                current = (x, y)
+
+                self.path_cost[current] = self.path_cost.get(parent, 0) + 1
+
+                f_score = self.path_cost[current] + self.heuristic(current)
+
+                self.to_explore.append((current, f_score))
+
+                self.path[current] = parent
+
+                index = len(self.to_explore) - 1
+                while index > 0:
+                    parent_index = (index - 1) // 2
+                    if self.to_explore[index][1] < self.to_explore[parent_index][1]:
+                        self.to_explore[index], self.to_explore[parent_index] = self.to_explore[parent_index], self.to_explore[index]
+                        index = parent_index
+                    else:
+                        break
+
+            def bubble_down(self, index: int):
+                size = len(self.to_explore)
+                while True:
+                    smallest = index
+                    left = 2 * index + 1
+                    right = 2 * index + 2
+
+                    if left < size and self.to_explore[left][1] < self.to_explore[smallest][1]:
+                        smallest = left
+
+                    if right < size and self.to_explore[right][1] < self.to_explore[smallest][1]:
+                        smallest = right
+
+                    if smallest != index:
+                        self.to_explore[index], self.to_explore[smallest] = self.to_explore[smallest], self.to_explore[index]
+                        index = smallest
+                    else:
+                        break
+
+        self.maze.matrix[self.playerX][self.playerY] = 1
+        self.playerY-=1
+        queue = PriorityQueue(self.playerX,self.playerY,self.maze.exitX,self.maze.exitY)
+        neighbors = [(self.playerX,self.playerY-1),(self.playerX-1,self.playerY),(self.playerX+1,self.playerY),(self.playerX,self.playerY+1)]
+        for i in neighbors:
+            neighbor_cost = queue.path_cost[(self.playerX,self.playerY)] + 1
+            if self.maze.matrix[i[0]][i[1]] == 1:
+                    continue
+            if (i[0],i[1]) not in queue.path_cost or queue.path_cost[(i[0],i[1])] > neighbor_cost:
+                queue.add_element((i[0],i[1],(self.playerX,self.playerY)))
+                self.maze.matrix[i[0]][i[1]] = 4
+
+        self.maze.matrix[self.playerX][self.playerY] = 3
+        self.maze.draw_maze(self.playerX,self.playerY,True)
+        visited = set()
+        while len(queue.to_explore) != 0:
+            self.maze.draw_maze(self.playerX,self.playerY,True)
+            current_node = queue.to_explore[0]
+            current_node = current_node[0]
+            if len(queue.to_explore) == 1:
+                queue.to_explore.pop()
+            else:
+                queue.to_explore[0] = queue.to_explore.pop()
+                queue.bubble_down(0)
+            
+            if current_node in visited:
+                continue
+            visited.add(current_node)
+
+            neighbors = [(current_node[0],current_node[1]-1),(current_node[0]-1,current_node[1]),(current_node[0]+1,current_node[1]),(current_node[0],current_node[1]+1)]
+            if current_node == (self.maze.exitX,self.maze.exitY):
+                next_cell = queue.path[current_node]
+                self.playerX,self.playerY = current_node[0],current_node[1]
+                self.maze.check_win(self.playerX,self.playerY)
+                while next_cell in queue.path:
+                    self.maze.matrix[next_cell[0]][next_cell[1]] = 2
+                    self.maze.draw_maze(self.playerX, self.playerY, True)
+                    next_cell = queue.path[next_cell]
+                break
+            
+            for i in neighbors:
+                neighbor_cost = queue.path_cost[current_node] + 1
+                if self.maze.matrix[i[0]][i[1]] == 1:
+                    continue
+                if (i[0],i[1]) not in queue.path_cost or queue.path_cost[(i[0],i[1])] > neighbor_cost:
+                    queue.add_element((i[0],i[1],current_node))
+                    self.maze.matrix[i[0]][i[1]] = 4
+
+
 
 running = True
 labirynt = Maze(ROWS, COLS,CELL_SIZE)
@@ -306,10 +407,10 @@ solver = Solver(labirynt)
 
 # solver.right_hand_solver()
 # solver.dfs_solver()
-solver.bfs_solver()
+# solver.bfs_solver()
 # solver.dead_end_solver()
 # solver.tremaux_solver()
-
+solver.a_star()
 
 while running:
     for event in pygame.event.get():
@@ -327,3 +428,5 @@ pygame.quit()
 #DEAD 75,75 AVG < 6
 #Tremaux 41,41 Best:0.209 Worst:1.11 AVG < 0.9
 #Tremaux 75,75 Best:4.334 Worst: 12,823 AVG < 10
+#A* 41,41
+#A* 75,75 Best: 2,925 Worst: 6.641
